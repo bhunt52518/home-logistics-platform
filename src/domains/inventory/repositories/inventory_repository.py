7d4cba90887266bbd2b522e3538f976c45d65878
bhunt52518 <bhunt52518@gmail.com>
2026-07-26 15:16:15 -1000

    feat: add item and inventory record repository integration tests

--- src/domains/inventory/repositories/inventory_repository.py
+++ src/domains/inventory/repositories/inventory_repository.py
@@ -2,6 +2,8 @@ from src.domains.inventory.models.inventory_record import InventoryRecord
 from src.domains.inventory.persistence.household_db import HouseholdDB
 from src.domains.inventory.persistence.category_db import CategoryDB
 from src.domains.inventory.persistence.location_db import LocationDB
+from src.domains.inventory.persistence.item_db import ItemDB
+from src.domains.inventory.persistence.inventory_record_db import InventoryRecordDB
 
 from sqlalchemy.orm import Session
 
@@ -35,3 +37,19 @@ def create_category(session: Session, category: CategoryDB) -> CategoryDB:
     session.refresh(category)
 
     return category
+
+def create_item(session: Session, item: ItemDB):
+
+    session.add(item)
+    session.commit()
+    session.refresh(item)
+
+    return item
+
+def create_inventory_record(session: Session, inventory_record: InventoryRecordDB):
+
+    session.add(inventory_record)
+    session.commit()
+    session.refresh(inventory_record)
+
+    return inventory_record
--- tests/test_inventory_persistence.py
+++ tests/test_inventory_persistence.py
@@ -4,10 +4,14 @@ from src.domains.inventory.persistence.household_db import HouseholdDB
 from src.domains.inventory.persistence.inventory_record_db import InventoryRecordDB
 from src.domains.inventory.persistence.item_db import ItemDB
 from src.domains.inventory.persistence.location_db import LocationDB
-from src.domains.inventory.repositories.inventory_repository import (create_household, create_category, create_location)
+from src.domains.inventory.repositories.inventory_repository import (create_household, create_category, create_location,
+                                                                     create_item, create_inventory_record)
 
 from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import Session
+
+from datetime import date
+from decimal import Decimal
 
 
 
@@ -83,3 +87,60 @@ def test_category_can_be_saved_and_retrieved() -> None:
         assert retrieved_category.id is not None
         assert retrieved_category.name =="Meat"
         assert retrieved_category.perishable_default is True
+
+def test_item_can_be_saved_and_retrieved() -> None:
+    engine = create_engine("sqlite:///:memory:")
+    Base.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        category = CategoryDB(name="meat", perishable_default=True)
+        saved_category = create_category(session=session, category=category)
+
+        item = ItemDB(name="Chicken", category_id=saved_category.id, default_unit="lbs")
+        saved_item = create_item(session=session, item=item)
+        retrieved_item = session.get(ItemDB, saved_item.id)
+
+        assert saved_item is not None
+        assert saved_item.id is not None
+        assert saved_item.name == "Chicken"
+
+        assert retrieved_item is not None
+        assert retrieved_item.id is not None
+        assert retrieved_item.name =="Chicken"
+
+def test_inventory_record_can_be_saved_and_retrieved() -> None:
+    engine = create_engine("sqlite:///:memory:")
+    Base.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        household = HouseholdDB(name="Hunt Family")
+        saved_household = create_household(session=session, household=household)
+
+        category = CategoryDB(name="Meat", perishable_default=True)
+        saved_category = create_category(session=session, category=category)
+
+        location = LocationDB(household_id=saved_household.id, name="Kitchen Refrigerator")
+        saved_location = create_location(session=session, location=location)
+
+        item = ItemDB(name="Chicken", category_id=saved_category.id, default_unit="lbs")
+        saved_item = create_item(session=session, item=item)
+
+        inventory_record = InventoryRecordDB(item_id=saved_item.id, location_id=saved_location.id, quantity=Decimal("2"),
+                                             unit=saved_item.default_unit, purchased_date=date(2025, 5, 27), expiration_date=None)
+        saved_inventory_record = create_inventory_record(session=session, inventory_record=inventory_record)
+        retrieved_inventory_record = session.get(InventoryRecordDB, saved_inventory_record.id)
+
+        assert saved_inventory_record is not None
+        assert saved_inventory_record.id is not None
+        assert saved_inventory_record.quantity == Decimal("2")
+        assert saved_inventory_record.purchased_date == date(2025, 5, 27)
+
+        assert retrieved_inventory_record is not None
+        assert retrieved_inventory_record.id is not None
+        assert retrieved_inventory_record.quantity == Decimal("2")
+        assert retrieved_inventory_record.item_id == saved_item.id
+        assert retrieved_inventory_record.location_id == saved_location.id
+        assert retrieved_inventory_record.unit == saved_item.default_unit
+        assert retrieved_inventory_record.purchased_date == date(2025, 5, 27)
+        assert retrieved_inventory_record.expiration_date is None
+
